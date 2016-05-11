5ed3ce226b1a9ae8cb6ede43f9535bcdbd16580d
Michael "Z" Goddard <mzgoddard@gmail.com>
2016-05-11 13:37:39 -0400

    Add boxart-animated package for publishing

---
+++ packages/boxart-animated/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  Animated: require('boxart/lib/animated'),
+  AnimatedAgent: require('boxart/lib/animated-agent'),
+  AnimatedRect: require('boxart/lib/animated-rect'),
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 module.exports = {
+  Animated: require('./animated'),
+  AnimatedAgent: require('./animated-agent'),
+  AnimatedRect: require('./animated-rect'),
   Stage: require('./stage'),
   CSSStage: require('./css-stage'),
   LinearStage: require('./linear-stage'),
