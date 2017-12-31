b0b6a90386a5ee43e6a1361c603f595f7b58a650
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-12-31 13:00:22 -0500

    Build babel versions of animate, present, and update

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 const FunctionCompilePlugin = require('./src/webpack/function-compile-plugin');
 
 const {join} = require('path');
@@ -7,9 +9,12 @@ const dir = (...args) => join(__dirname, ...args);
 module.exports = {
   context: dir(),
   entry: {
-    'animate': './src/level0/animate',
-    'present': './src/level0/present',
-    'update': './src/level0/update',
+    // 'animate': './src/level0/animate',
+    // 'present': './src/level0/present',
+    // 'update': './src/level0/update',
+    'animate.compile': './src/level0/animate.compile',
+    'update.compile': './src/level0/update.compile',
+    'present.compile': './src/level0/present.compile',
   },
   output: {
     path: dir('tmp/level0'),
@@ -22,5 +27,8 @@ module.exports = {
   },
   plugins: [
     new FunctionCompilePlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"',
+    }),
   ],
 };
