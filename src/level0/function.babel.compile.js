5e7c5f384547a193cd259b2a7e4f566771c27c4d
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-12-31 12:48:07 -0500

    Improve babel compiled inlining with runtime meta data
    - Split out arrow to function expression plugin
    - Cache asts of functions for injecting so calls to it can be inlined
    - Use CompileRegistry's meta data to deeply produce the function
      factories used in a factory being compiled or the output of a factory
    - Try and test if the compiled factory or factory output can be
      evaluated. If not try and fallback to the original function and if
      something tries to stringify the function, throw an Error.

--- src/level0/function.babel.compile.js
+++ src/level0/function.babel.compile.js
@@ -2,24 +2,36 @@ import {transform} from 'babel-standalone';
 
 import plugin from './function.babel';
 
+function _arrowToFunction() {
+  return {
+    visitor: {
+      ArrowFunctionExpression(path) {
+        path.arrowFunctionToShadowed();
+      },
+    },
+  };
+}
+
 function _fillInFunctions(registry) {
+  const asts = {};
   return function({types: t}) {
     return {
       visitor: {
-        ArrowFunctionExpression(path) {
-          path.arrowFunctionToShadowed();
-        },
-
         CallExpression(path) {
           if (
             t.isIdentifier(path.node.callee) &&
             !path.scope.getBinding(path.node.callee.name)
           ) {
             if (registry[path.node.callee.name]) {
-              const code = registry[path.node.callee.name].toString();
-              const {ast} = transform(code, {});
+              let ast = asts[path.node.callee.name];
+              if (!ast) {
+                const code = registry[path.node.callee.name].toString();
+                ast = transform(code, {}).ast;
+                asts[path.node.callee.name] = ast;
+              }
               if (t.isFunctionDeclaration(ast.program.body[0])) {
-                path.getStatementParent().insertBefore(ast.program.body[0]);
+                path.getStatementParent()
+                .insertBefore(t.cloneDeep(ast.program.body[0]));
               }
             }
           }
@@ -29,16 +41,60 @@ function _fillInFunctions(registry) {
   };
 }
 
+function _stringifyPreludeArgs(args) {
+  try {
+    return args
+      .map(function(arg) {
+        if (arg && arg.meta) {
+          return [
+            _stringifyPrelude(arg.meta.origin),
+            _stringifyPreludeArgs(arg.meta.args),
+            String(arg.meta.origin),
+          ]
+          .filter(Boolean)
+          .join(';');
+        }
+        return _stringifyPrelude(arg);
+      })
+      .filter(Boolean)
+      .join(';')
+  }
+  catch (e) {}
+  return '';
+}
+
+function _stringifyPrelude(fn) {
+  if (typeof fn === 'function') {
+    try {
+      const result = fn();
+      if (typeof result === 'function' && result.meta) {
+        return `${_stringifyPrelude(result.meta.origin)};
+          ${_stringifyPreludeArgs(result.meta.args)};
+          ${String(result.meta.origin)};`;
+      }
+    }
+    catch (e) {}
+  }
+  if (fn && typeof fn === 'object') {
+    if (Array.isArray(fn)) {
+      return _stringifyPreludeArgs(fn);
+    }
+    else if (fn.constructor.name === 'Object') {
+      return _stringifyPreludeArgs(Object.values(fn));
+    }
+  }
+  return '';
+}
+
 function _stringify(fn) {
   if (typeof fn === 'function') {
     if (fn.meta) {
-      return `${fn.meta.origin.toString()}(${
+      return `${_stringify(fn.meta.origin)}(${
         fn.meta.args.map(_stringify).join(', ')
       })`;
     }
-    else {
-      return fn.toString();
-    }
+
+    return fn.toString();
   }
   else if (fn && typeof fn === 'object') {
     if (Array.isArray(fn)) {
@@ -52,16 +108,68 @@ function _stringify(fn) {
       .join(', ')
     }}`;
   }
+  else if (typeof fn === 'undefined') {
+    return 'null';
+  }
   return JSON.stringify(fn);
 }
 
 export default function compile(fn, registry) {
-  const body = `function compiled() {return ${_stringify(fn)}}`;
+  // Build the factory factory for what fn can do. Given the meta information
+  // stored by compile-registry, write all the functions used to build up fn.
+  // And finally call the top function with all its arguments and the arguments
+  // arguments.
+  const body = `function compiled() {${_stringifyPrelude(fn)}; return ${_stringify(fn)}}`;
+  // "Safety" pass.
   const result1 = transform(body, {
-    plugins: [_fillInFunctions(registry)]
+    plugins: [
+      _arrowToFunction,
+      // Back up in case functions were not wrapped with `inlined`. They may be
+      // available in the registry for the top function.
+      _fillInFunctions(registry),
+    ],
   });
+  // "Real" pass. Inline functions and try to statically remove unnecessary
+  // operations.
   const result = transform(result1.code, {
     plugins: [plugin]
   });
-  return new Function(`return ${result.code}`)();
+  // Return a function that instantiates the compiled instance. If it cannot be
+  // instantiated, the original will be used in its stead and writing the
+  // compiled version to a string will throw an Error.
+  return function() {
+    let f;
+    try {
+      f = new Function(`return ${result.code}`)()();
+      if (!fn.meta) {
+        // Functions with meta are the result of a boxart function factory.
+        // Without meta, we know we are given a factory. Try calling the
+        // factory. If there is an error we shall assume it is not safe to
+        // stringify it.
+        f();
+      }
+    }
+    catch (e) {
+      // Errors other than ReferenceError's are fine if f was able to be
+      // generated.
+      if (f && !/(\w+) is not defined/.test(e.message)) {
+        return f;
+      }
+      // An error occured testing the compiled source. The original function can
+      // be safely called.
+      f = function(...args) {
+        return fn(...args);
+      };
+      // Stringify'ing the original function wouldn't be tremendously useful.
+      // It'd only be a part of the function set for a given animation
+      // component.
+      f.toString = function() {
+        throw new Error(
+          `Could not compile a boxart function. A function may not have been ` +
+          `able to be inlined.\nOriginal Error:\n${e.stack || e}`
+        );
+      };
+    }
+    return f;
+  };
 };
