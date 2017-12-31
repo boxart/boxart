7d3765e5fb8a240994dff24a689394c4d57519af
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-12-31 12:43:17 -0500

    Add CompileRegistry.registry and inlined
    - registry returns a frozen copy of the registry so the original
      functions can be viewed or otherwise used but modifications to the
      registry cannot be made
    - inlined wraps a function to record the function and the passed
      arguments on the returned result

--- src/level0/compile-registry.js
+++ src/level0/compile-registry.js
@@ -1,6 +1,58 @@
 const compile = require('./function.babel.compile').default;
 const funcRegistry = require('./function-registry');
 
+const lazyCompile = (fn, registry) => {
+  fn._compiled = fn._compiled || compile(fn, registry)();
+  return fn._compiled;
+};
+
+const lazyCall = (fn, registry) => (...args) => {
+  return lazyCompile(fn, registry)(...args);
+};
+
+const lazyKeyCall = (fn, key, registry) => (...args) => {
+  return lazyCompile(fn, registry)[key](...args);
+};
+
+const clearNesting = args => {
+  args.forEach(a => {
+    if (typeof a === 'function' && !(a instanceof Function)) {
+      Object.setPrototypeOf(a, Function.prototype);
+    }
+  });
+};
+
+function bindDefinition(fn, autobind, registry, origin, args) {
+  if (!autobind) {return fn;}
+  try {
+    const wrapped = Object.assign(lazyCall(fn), fn);
+    // Object.defineProperty(fn, 'meta', {
+    //   value: {origin, args, meta: fn.meta},
+    //   enumerable: false,
+    // });
+    Object.defineProperty(wrapped, 'meta', {
+      value: {origin, args},
+      enumerable: false,
+    });
+    Object.keys(fn).forEach(function(key) {
+      wrapped[key] = lazyKeyCall(fn, key);
+    });
+    return wrapped;
+  } catch(e) {
+    console.error(e);
+    return fn;
+  }
+}
+
+function inlined(fn, registry) {
+  const f = function(...args) {
+    clearNesting(args);
+    return bindDefinition(fn.call(this, ...args), true, registry, fn, args);
+  };
+  f.__boxart_inlined = fn;
+  return f;
+}
+
 const create = (fn, constructor = {}) => {
   function PresentFunction(f, original) {
     if (original) {
@@ -13,6 +65,10 @@ const create = (fn, constructor = {}) => {
   PresentFunction.prototype = Object.create(Function.prototype);
   PresentFunction.prototype.constructor = PresentFunction;
 
+  PresentFunction.prototype.toString = function() {
+    return this.asfunc().toString();
+  };
+
   function PresentObject(f) {
     return Object.setPrototypeOf(f, PresentObject.prototype);
   }
@@ -22,52 +78,11 @@ const create = (fn, constructor = {}) => {
   PresentObject.prototype.compile = function() {
     return compile(this, registry)();
   };
-
-  const clearNesting = args => {
-    args.forEach(a => {
-      if (Object.getPrototypeOf(a) === PresentObject.prototype) {
-        Object.setPrototypeOf(a, Function.prototype);
-      }
-    });
-  };
-
-  const lazyCompile = (fn) => {
-    fn._compiled = fn._compiled || compile(fn, registry)();
-    return fn._compiled;
-  };
-
-  const lazyCall = (fn) => (...args) => {
-    return lazyCompile(fn)(...args);
-  };
-
-  const lazyKeyCall = (fn, key) => (...args) => {
-    return lazyCompile(fn)[key](...args);
+  PresentObject.prototype.toString = function() {
+    return this.compile(this).toString();
   };
 
   let autobind = true;
-
-  function bindDefinition(fn, origin, args) {
-    if (!autobind) {return fn;}
-    try {
-      const wrapped = Object.assign(lazyCall(fn), fn);
-      Object.defineProperty(fn, 'meta', {
-        value: {origin, args},
-        enumerable: false,
-      });
-      Object.defineProperty(wrapped, 'meta', {
-        value: {origin, args},
-        enumerable: false,
-      });
-      Object.keys(fn).forEach(function(key) {
-        wrapped[key] = lazyKeyCall(fn, key);
-      });
-      return wrapped;
-    } catch(e) {
-      console.error(e);
-      return fn;
-    }
-  }
-
   const registry = {};
   const present = Object.create(constructor);
   Object.defineProperties(present, {
@@ -102,20 +117,23 @@ const create = (fn, constructor = {}) => {
         if (args) {
           fn.args = args;
         }
+        if (fn.__boxart_inlined) {
+          fn = fn.__boxart_inlined;
+        }
         registry[key] = fn;
         // if (process.env.NODE_ENV !== 'production' && registry[key] !== fn) {
         //   throw new Error('Cannot register on a frozen animation builder');
         // }
         present[key] = new PresentFunction(function(...args) {
           clearNesting(args);
-          return new PresentObject(bindDefinition(fn.call(this, ...args), fn, args));
+          return new PresentObject(bindDefinition(fn.call(this, ...args), autobind, registry, fn, args));
         }, fn);
         PresentObject.prototype[key] =
           PresentFunction.prototype[key] =
           new PresentFunction(function(..._args) {
             const args = [this].concat(_args);
             clearNesting(args);
-            return new PresentObject(bindDefinition(fn.call(this, ...args), fn, args));
+            return new PresentObject(bindDefinition(fn.call(this, ...args), autobind, registry, fn, args));
           });
         return present[key];
       },
@@ -132,6 +150,12 @@ const create = (fn, constructor = {}) => {
       enumerable: false,
       value(fn) {
         return fn(present);
+      },
+    },
+    registry: {
+      enumerable: false,
+      value() {
+        return Object.freeze(Object.assign({}, registry));
       },
     },
     freeze: {
@@ -166,3 +190,4 @@ const create = (fn, constructor = {}) => {
 
 module.exports = create;
 module.exports.create = create;
+module.exports.inlined = inlined;
