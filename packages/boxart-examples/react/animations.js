821dc1a0970e4849b26859e77a0659f19bda6d9f
Michael "Z" Goddard <mzgoddard@gmail.com>
2018-02-20 16:36:54 -0500

    Add enter/leave transition use to the react example

--- packages/boxart-examples/react/animations.js
+++ packages/boxart-examples/react/animations.js
@@ -5,11 +5,44 @@ export default {
     default: {
       update: update.rect(),
       animate: animate.object({
+        top: animate.begin().to(animate.end()),
         left: animate.begin().to(animate.end()),
+        height: animate.begin().to(animate.end()),
       }),
       present: present.style({
-        transform: present.translatex([
-          present.key('left').to(present.end).px(),
+        transform: present.concat([
+          present.translate([
+            present.key('left').to(present.end).px(),
+            present.key('top').to(present.end).px(),
+          ]),
+          present.scaley([
+            present.key('height').over(present.end),
+          ]),
+        ]),
+      }),
+    },
+    enter: {
+      animate: animate.object({
+        top: animate.begin().to(animate.end()),
+        left: animate.begin().to(animate.end()),
+        height: animate.constant(0).to(animate.end()),
+      }),
+    },
+    leave: {
+      animate: animate.object({
+        top: animate.begin().to(animate.end()),
+        left: animate.begin().to(animate.end()),
+        height: animate.begin().to(animate.constant(0)),
+      }),
+      present: present.style({
+        transform: present.concat([
+          present.translate([
+            present.key('left').to(present.end).px(),
+            present.key('top').to(present.end).px(),
+          ]),
+          present.scaley([
+            present.key('height').over(present.begin),
+          ]),
         ]),
       }),
     },
--- packages/boxart-examples/react/index.js
+++ packages/boxart-examples/react/index.js
@@ -5,9 +5,10 @@ import BoxArt from 'boxart-react-dom';
 
 import animations from './animations';
 
-function Box({onClick}) {
+function Box({id, onClick}) {
   return <div
-    className="box box1"
+    data-id={id}
+    className={`box box${id}`}
     style={{width: '100px', height: '100px', background: 'blue'}}
     onClick={onClick}
     />;
@@ -22,23 +23,55 @@ function Column({children}) {
 class App extends React.Component {
   constructor(...args) {
     super(...args);
-    this.state = {i: 0};
+    this.state = {columnA: ['00'], columnB: [], count: 0};
+    this.addOrDelete = this.addOrDelete.bind(this);
     this.swap = this.swap.bind(this);
   }
 
-  swap() {
-    this.setState({i: 1 - this.state.i});
+  addOrDelete() {
+    const {columnA, columnB, count} = this.state;
+    if ((count % 8) < 4) {
+      const column = ['columnA', 'columnB'][Math.floor(Math.random() * 2)];
+      this.setState({
+        [column]: this.state[column].concat(count.toString()),
+        count: count + 1,
+      });
+    }
+    else {
+      let index = Math.floor(Math.random() * [...columnA, ...columnB].length);
+      const column = index < columnA.length ? 'columnA' : 'columnB';
+      index += index < columnA.length ? 0 : -columnA.length;
+      this.setState({
+        [column]:
+          this.state[column].slice(0, index).concat(this.state[column].slice(index + 1)),
+        count: count + 1,
+      });
+    }
+  }
+
+  swap(event) {
+    const id = event.currentTarget.dataset.id;
+    const column = this.state.columnA.indexOf(id) !== -1 ? 'columnA' : 'columnB';
+    const index = this.state[column].indexOf(id);
+    const otherColumn = column === 'columnA' ? 'columnB' : 'columnA';
+    this.setState({
+      [column]: this.state[column].slice(0, index).concat(this.state[column].slice(index + 1)),
+      [otherColumn]: this.state[otherColumn].concat(id),
+    });
+    event.preventDefault();
+    event.stopPropagation();
+    return true;
   }
 
   render() {
-    const {i} = this.state;
+    const {columnA, columnB} = this.state;
     const swap = this.swap;
-    return (<div>
+    return (<div style={{position: 'absolute', top: 0, bottom: 0, left: 0, right: 0}} onClick={this.addOrDelete}>
       <Column>
-        {i === 0 ? <Box onClick={swap} /> : null}
+        {columnA.map(id => <Box key={id} id={id} onClick={swap} />)}
       </Column>
       <Column>
-        {i === 1 ? <Box onClick={swap} /> : null}
+        {columnB.map(id => <Box key={id} id={id} onClick={swap} />)}
       </Column>
     </div>);
   }
