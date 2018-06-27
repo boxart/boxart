760df2c55a87e1592d05d2ffb6a2d585dd22f6a6
Michael "Z" Goddard <mzgoddard@gmail.com>
2018-06-27 09:09:51 -0400

    add rotate-in-right animation

--- packages/boxart-presentation-preact/slides/02-02.js
+++ packages/boxart-presentation-preact/slides/02-02.js
@@ -2,7 +2,7 @@ import {h} from 'preact';
 
 import {Render} from 'boxart-factory-preact';
 
-export default <div class="slide 01-04" style={{height: "100%"}}>
+export default <div class="slide 01-04 rotate-in-right" style={{height: "100%"}}>
   <Render
     rect={require('../templates/title')}
     insert={{title: 'title3'}}
